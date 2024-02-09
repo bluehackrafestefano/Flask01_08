241892636dc9a10d5a523e6d27b66dd2575a7ca5
Rafe <stefanorafe@gmail.com>
2024-02-09 21:44:59 +0100

    adds a new endpoint

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/products")
+def products():
+    return "<h1>Product Page</h1>"
+
 if __name__ == '__main__':
     # app.run(debug=True)
     # app.run(debug=True, port=3003)
